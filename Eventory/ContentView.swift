735d189ce46f33131cb9646a6f62c47c4f9f89ef
Jos van Oorschot <josvanoorschot7759@gmail.com>
2021-02-16 16:38:52 +0100

    Initial Version of Eventory App

---
+++ Eventory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eventory
+//
+//  Created by jos van oorschot on 16/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eventory/EventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoryApp.swift
+//  Eventory
+//
+//  Created by jos van oorschot on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct EventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
